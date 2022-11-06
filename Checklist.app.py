446eed1f6bb077a1eb7bfbabd8f4649366d25edb
mhrrosa <71407848+mhrrosa@users.noreply.github.com>
2022-11-05 21:28:49 -0300

    Update Checklist.app.py

--- Checklist.app.py
+++ Checklist.app.py
@@ -3,11 +3,13 @@ import random
 from collections import OrderedDict
 import re
 import string
+from time import sleep
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 from xml.etree import ElementTree
 import pandas
 from unidecode import unidecode
+import PySimpleGUI as psg
 
 
 
@@ -26,34 +28,71 @@ def armazena_perguntas(file_name_xlsx):
         prioridade_peso = dados[1]['PRIORIDADE']
 
         #armazenando no dict
-        dict_perguntas[id] = {
-            "descricao": descricao,
+        dict_perguntas[descricao] = {
+            "id": id,
             "responsavel": responsavel,
             "prioridade": prioridade_peso
         }
     return dict_perguntas
 
 def calcula_aderencia(dict_perguntas):
-    print("ATENÇÃO: RESPONDA SOMENTE COM A LETRA S OU N")
-    for id in dict_perguntas:
-        # declarando variaveis e armazenando seu valores
-        resposta = ""
-        pergunta = dict_perguntas[id]['descricao']
-        responsavel = dict_perguntas[id]['responsavel']
-        prioridade = dict_perguntas[id]['prioridade']
 
-        while resposta.upper() != "N" and resposta.upper() != "S":
-                resposta = input(f"{pergunta} [S/N]").upper()
-
-        if resposta.upper() == "N":
-            justificativa = input(f"insira uma justificativa para a NFC: ")
-            f.write(f"ID: {id}\n")
-            f.write(f"DESCRICAO: {pergunta}\n")
-            f.write(f"RESPONSAVEL: {responsavel}\n")
-            f.write(f"PRIORIDADE: {prioridade}\n")
-            f.write(f"JUSTIFICATIVA NFC: {justificativa}\n")
+    lista_perguntas =[]
+    contagem = 0
+    for key in dict_perguntas.keys():
+        lista_perguntas.append(key)
 
 
+    # define o tema da tela
+    psg.theme('reddit')
+
+    layout = [
+        [psg.Text(size=(70, 1))],
+        [psg.Text(f"{lista_perguntas[contagem]}", size=(300, 2), font='Arial 12', justification='center', key='text',
+                  text_color='black')],
+        [psg.Radio('SIM', 1, enable_events=True, key='R1'), psg.Radio('NÃO', 1, enable_events=True, key='R2')],
+        [psg.Text(size=(70, 1))],
+        [psg.Multiline(size=(100, 5),font='Arial 12', key='textbox', disabled=True, visible=False)],
+        [psg.Button('testar', border_width=2, size=(15, 1))],
+
+    ]
+    window = psg.Window('test', layout, size=(1000, 300), element_justification='c')
+
+    while True:
+        # le os valores e os eventos
+        event, values = window.read()
+
+        if event is None:
+            break
+        elif values['R1'] == True:
+            window['textbox'].update(disabled=True)
+            window['textbox'].update(visible=False)
+        elif values['R2'] == True:
+            window['textbox'].update(disabled=False)
+            window['textbox'].update(visible=True)
+        if event == 'testar':
+
+            try:
+                window['text'].update(lista_perguntas[contagem])
+            except:
+                break
+            if values['R2'] == True:
+                f.write(f"ID: {dict_perguntas[lista_perguntas[contagem]]['id']}\n")
+                f.write(f"DESCRICAO: {lista_perguntas[contagem]}\n")
+                f.write(f"RESPONSAVEL: {dict_perguntas[lista_perguntas[contagem]]['responsavel']}\n")
+                f.write(f"PRIORIDADE: {dict_perguntas[lista_perguntas[contagem]]['prioridade']}\n")
+                f.write(f"JUSTIFICATIVA NFC: {values['textbox']}\n")
+
+
+            window['textbox'].update("")
+            window['textbox'].update(disabled=True)
+            window['textbox'].update(visible=False)
+            window['R1'].update(False)
+            window['R2'].update(False)
+            contagem+=1
+
+
+    print('rs')
 def main():
     # selecionando arquivos
     print('Selecione a planilha')
